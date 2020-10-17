290b156aaa06ebe1ac03ed664a4c4df4aed3cc34
Hosam Hamed <38729340+hosam-hamed-33@users.noreply.github.com>
2020-10-16 22:00:31 -0400

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ global $CFG;
 $CFG = new stdClass();
 
 $CFG->dbtype    = getenv('pgsql');
-$CFG->dblibrary = 'dekfu3lju68ui1';
+$CFG->dblibrary = 'postgresql-objective-96403';
 $CFG->dbhost    = getenv('ec2-3-95-87-221.compute-1.amazonaws.com');
 $CFG->dbname    = getenv('dekfu3lju68ui1');
 $CFG->dbuser    = getenv('vpbzgxaqspzbst');
